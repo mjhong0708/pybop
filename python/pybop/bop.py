9cb83e6a033cf8009ee106a47cbbbcf9026c9423
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2022-02-17 00:50:49 +0900

    updated

--- python/pybop/bop.py
+++ python/pybop/bop.py
@@ -1,9 +1,31 @@
+from typing import Sequence, Union
+
+import numpy as np
+from ase import Atoms
 from joblib import Parallel, delayed
-from .pybop import calculate_bop as _bop_rs
+
 from .neighbors import get_neighbor_positions
+from .pybop import calculate_bop as _bop_rs
 
 
-def calculate_bop(atoms, r_cut, params_list, n_jobs=1):
+def calculate_bop(
+    atoms: Atoms,
+    r_cut: float,
+    params_list: Union[Sequence[Sequence[float]], np.ndarray],
+    n_jobs: int = 1,
+) -> float:
+    """Calculates BOP for an Atoms object.
+
+    Args:
+        atoms (Atoms): Atoms object
+        r_cut (float): cutoff radius in Angstrom
+        params_list (Union[Sequence[Sequence[float]], np.ndarray]): list of local BOP parameters.
+            Each element is a list of 8 parameters. (N, 8) numpy array is also accepted.
+        n_jobs (int, optional): The number of parallel jobs. Defaults to 1.
+
+    Returns:
+        float: energy of Atoms
+    """
     center_positions = atoms.positions
     neighbors_positions = get_neighbor_positions(atoms, r_cut)
     energy = 0
@@ -12,10 +34,6 @@ def calculate_bop(atoms, r_cut, params_list, n_jobs=1):
             delayed(_bop_rs)(center_positions[i], neighbors_positions[i], params_list[i], r_cut)
             for i in range(len(atoms))
         )
-        # atomic_energies = (
-        #     _bop_rs(center_positions[i], neighbors_positions[i], params_list[i], r_cut)
-        #     for i in range(len(atoms))
-        # )
         energy = sum(atomic_energies)
     else:
         for center, neighbors, params in zip(center_positions, neighbors_positions, params_list):
---
+++ test/test_calculation.py
@@ -0,0 +1,11 @@
+import numpy as np
+from ase.build import bulk
+from pybop.bop import calculate_bop
+
+atoms = bulk("atoms", "fcc", a=3.92, cubic=True)
+atoms = atoms.repeat((3, 3, 3))  # Supercell
+
+bop_params = 0.5 * np.ones((len(atoms), 8), dtype=np.float64)
+
+energy = calculate_bop(atoms, r_cut=6.5, params_list=bop_params, n_jobs=8)
+print(energy)
